8a6624ebd8b6a5b5615949affacc6bee5e1e1603
Lorentyfle <106763902+Lorentyfle@users.noreply.github.com>
2026-09-04 10:53:43 +0200

    Update the packagelock

--- extension.js
+++ extension.js
@@ -1041,8 +1041,8 @@ function activate(context) {
           for (const name of cachedCharacterNames) {
             items.push(createCharacterCompletion(name));
           }
-          return createWordSuggestions(document, beforeCursor);
-          return items
+          items.push(createWordSuggestions(document, beforeCursor));
+          return items;
           }
         }
     , ' ', '[');
